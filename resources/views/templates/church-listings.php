64824b4deae47bf69e602d4069dadea91f34815b
Benjamin Locher <blocher@gmail.com>
2015-07-19 22:33:01 -0400

    fixing html encoding issue

--- resources/views/templates/church-listings.php
+++ resources/views/templates/church-listings.php
@@ -1,6 +1,6 @@
 <script id="church-listings" type="text/x-handlebars-template">
   <div class="list-group">
-      <div class="alert alert-success"><h4>Your closest church is ... <a target="_blank" href="{{ churches.0.url }}">{{ churches.0.name }}</a><strong></strong></h4></div>
+      <div class="alert alert-success"><h4>Your closest church is ... <a target="_blank" href="{{ churches.0.url }}">{{{ churches.0.name }}}</a><strong></strong></h4></div>
       <!-- <div class="alert alert-info"><p>Your are likely in the </p></div> --> 
       <h4>Closest churches...</h4>
        {{#each churches }}
